c585b056807e33ba592cbbaa5e54beabd5f8c467
rum-n <manev.ru@gmail.com>
2020-03-23 14:28:53 +0200

    Updates

---
+++ precache-manifest.e88eeddf76adf763272054ffcdb1dc83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "592daf6f62fc4f361160",
+    "url": "/corona-quiz/static/css/main.b69cffa3.chunk.css"
+  },
+  {
+    "revision": "592daf6f62fc4f361160",
+    "url": "/corona-quiz/static/js/main.592daf6f.chunk.js"
+  },
+  {
+    "revision": "5a80405e8fff7c81f25f",
+    "url": "/corona-quiz/static/js/1.5a80405e.chunk.js"
+  },
+  {
+    "revision": "3d151cc3847c03b102d1",
+    "url": "/corona-quiz/static/js/runtime~main.3d151cc3.js"
+  },
+  {
+    "revision": "9f90edca1c0ae5cbb6a118d5c1b22e2c",
+    "url": "/corona-quiz/static/media/logo.9f90edca.png"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/corona-quiz/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "40aa644df3c38565ddb8c8e4c8f3576a",
+    "url": "/corona-quiz/index.html"
+  }
+];
